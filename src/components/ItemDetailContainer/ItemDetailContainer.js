7a492a92f5210e5ed1ba1162d7eef3a23e1c1693
sabricanale <sabrii.canale@gmail.com>
2022-09-28 15:27:46 -0300

    Desafío11

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -2,30 +2,21 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ItemDetail from "../ItemDetail/ItemDetail";
+import {doc, getDoc, getFirestore} from 'firebase/firestore'
 
 
-
-const products = [
-    {id: 1, image: 'https://http2.mlstatic.com/D_NQ_NP_951718-MLA49975814105_052022-O.webp' , title:"Remera lisa", price:2500, category: "Indumentaria", description: 'Remera lisa de algódon. Variedad de colores y talles', stock: 5}, {id: 2, image:'https://http2.mlstatic.com/D_NQ_NP_829809-MLA45145681623_032021-O.webp' , title:"Llavero", price:800, category: "Accesorios", description: 'Llavero metalizado. Variedad de colores', stock: 10}, {id: 3, image:'https://http2.mlstatic.com/D_NQ_NP_823318-MLA51357923861_082022-O.webp' , title:"Gorra", category: "Accesorios", price:2000, description: 'Gorra lisa sin estampa. Variedad de colores', stock: 4}]
-    
 const ItemDetailContainer = () => {
-
-    const [data, setData] = useState({})
-    const { detalleId } = useParams()
+	const [data, setData] = useState({});
+	const { detalleId } = useParams();
 
     useEffect(() =>{
-        const getData = new Promise(resolve =>{
-            setTimeout(()=>{
-                resolve(products)
-            })
-        })
-        getData.then(res => setData(res.find(product => product.id === parseInt(detalleId))))
-    }, [detalleId])
-    
-    return (
-        <ItemDetail data = {data}/>      
-    )
-}
+    const querydb = getFirestore();
+    const queryDoc = doc(querydb, 'products', detalleId);
+    getDoc(queryDoc).then ((res) => setData({id: res.id, ...res.data()} ));
+}, [detalleId] );
 
-export default ItemDetailContainer
+	return  <ItemDetail data = {data} />
 
+};
+
+export default ItemDetailContainer;
---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD3OQIkLDBIeZg4DCYEYEoyO-kJhP8aDic",
+  authDomain: "tiendarastro-285f9.firebaseapp.com",
+  projectId: "tiendarastro-285f9",
+  storageBucket: "tiendarastro-285f9.appspot.com",
+  messagingSenderId: "923638967832",
+  appId: "1:923638967832:web:314a679989de8385fddd6d",
+  measurementId: "G-L65W9Y8FDF"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);

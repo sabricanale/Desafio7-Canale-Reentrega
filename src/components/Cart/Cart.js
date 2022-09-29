94e21eb04d3fa903e538e0a615b3a46a0d9db2a4
sabricanale <sabrii.canale@gmail.com>
2022-09-29 17:45:07 -0300

    Desafío12

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,10 +3,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../Context/CartContext";
 import ItemCart from "../ItemCart/ItemCart";
+import { getFirestore, addDoc, collection } from "firebase/firestore";
 
 const Cart = () => {
 	const { cart, totalPrice } = useCartContext();
-	
+
+	const order = {
+		buyer: {
+			name: 'Sabrina',
+			email: 'sabricanale@gmail.com',
+			phone: '3513451127',
+			address: 'Ayacucho 1406'
+		},	
+		items:cart.map(product => ({id: product.id, title: product.title, price: product.price, quantity: product.quantity})),
+		total: totalPrice()		
+	}
+
+
+const handleClick = () => {
+	const db = getFirestore();
+	const orderCollection = collection (db, 'orders');
+	addDoc(orderCollection, order)
+	.then(({id}) => console.log(id));
+}
 
 	if (cart.length === 0) {
 		return (
@@ -27,7 +46,9 @@ const Cart = () => {
 			))}
 			<div className="textTotal">
 				<p>Total a pagar: ${totalPrice()}</p>
-		</div>
+				<button onClick={handleClick} >Realizar compra</button>
+			</div>
+			
 		</>
 	);
 };
